5b85d6255c8ad6a229c0105060edfb657efadc7d
ITkah <ilatkacenko132@gmail.com>
2021-03-22 20:41:16 +0200

    qwe

--- js/common.js
+++ js/common.js
@@ -11,5 +11,36 @@ $(".unchor").on("click", function(event) {
     event.preventDefault();
     var id = $(this).attr('href'),
         top = $(id).offset().top;
-    $('body,html').animate({ scrollTop: top }, 1500);
+    $('body,html').animate({ scrollTop: top }, 800);
+});
+
+var swiper = new Swiper('.partner_slider', {
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: '.right',
+        prevEl: '.left',
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        990: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 7,
+            spaceBetween: 30,
+        },
+    }
 });
